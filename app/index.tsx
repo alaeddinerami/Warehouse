ac07df79e46f025c8b4aa9f4b534c9102c7f08ad
alaeddinerami <alaeddinerami7@gmail.com>
2025-02-11 00:02:12 +0100

    feat(auth): implement login screen with form validation and API integration

--- app/index.tsx
+++ app/index.tsx
@@ -8,22 +8,25 @@ import {
   KeyboardAvoidingView,
   Platform,
   ScrollView,
+  Alert,
+  ActivityIndicator,
 } from 'react-native';
 import { StatusBar } from 'expo-status-bar';
 import { SafeAreaView } from 'react-native-safe-area-context';
 import * as yup from 'yup';
+import apiClient from './(utils)/api';
+import { router } from 'expo-router';
 
 const loginSchema = yup.object().shape({
-    username: yup.string().trim()
-    .required("Full Name is required.")
-    .matches(/^[A-Za-z\s]+$/, "Full Name cannot contain numbers or special characters.")
-    .min(2, "Full Name must be at least 2 characters")
-    .max(50, "Full Name cannot exceed 50 characters"),
-    secretKey: yup
-      .string()
-      .required('secretKey is required')
-     
-  });
+  username: yup
+    .string()
+    .trim()
+    .required('Full Name is required.')
+    .matches(/^[A-Za-z\s]+$/, 'Full Name cannot contain numbers or special characters.')
+    .min(2, 'Full Name must be at least 2 characters')
+    .max(50, 'Full Name cannot exceed 50 characters'),
+  secretKey: yup.string().required('secretKey is required'),
+});
 
 interface SocialButtonProps {
   icon: any;
@@ -31,11 +34,10 @@ interface SocialButtonProps {
 }
 
 const SocialButton: React.FC<SocialButtonProps> = ({ icon, onPress }) => (
-  <TouchableOpacity 
-    className="w-12 h-12 rounded-full bg-gray-800 justify-center items-center mx-2"
-    onPress={onPress}
-  >
-    <Image source={icon} className="w-6 h-6" />
+  <TouchableOpacity
+    className="mx-2 h-12 w-12 items-center justify-center rounded-full bg-gray-800"
+    onPress={onPress}>
+    <Image source={icon} className="h-6 w-6" />
   </TouchableOpacity>
 );
 
@@ -44,48 +46,59 @@ export default function LoginScreen() {
   const [secretKey, setsecretKey] = useState('');
   const [showsecretKey, setShowsecretKey] = useState(false);
   const [errors, setErrors] = useState<{ [key: string]: string }>({});
+  const [loading, setLoading] = useState(false);
 
   const handleLogin = async () => {
     try {
       await loginSchema.validate({ username, secretKey }, { abortEarly: false });
-      
       setErrors({});
-      console.log('Login with:', username, secretKey);
+      setLoading(true);
+
+      const { data } = await apiClient.get('/warehousemans', {
+        params: {
+          name: username,
+          secretKey: secretKey,
+        },
+      });
       
+      const user = data[0];
+      router.push('/(tabs)')
     } catch (err) {
-      if (err instanceof yup.ValidationError) {
-        const errorMessages: { [key: string]: string } = {};
-        err.inner.forEach(error => {
-          if (error.path) {
-            errorMessages[error.path] = error.message;
-          }
-        });
-        setErrors(errorMessages);
+        if (err instanceof yup.ValidationError) {
+          const errorMessages: { [key: string]: string } = {};
+          err.inner.forEach(error => {
+            if (error.path) errorMessages[error.path] = error.message;
+          });
+          setErrors(errorMessages);
+        } else {
+          Alert.alert('Authentication failed');
+        }
+      } finally {
+        setLoading(false);
       }
-    }
   };
 
   return (
-    <SafeAreaView className="flex h-full justify-center pb-10 bg-[#1a1a1a]">
+    <SafeAreaView className="flex h-full justify-center bg-[#1a1a1a] pb-10">
       <StatusBar style="light" />
 
       <View className="flex">
-        <View className="items-center mb-8 px-6">
+        <View className="mb-8 items-center px-6">
           <Image
             source={require('../assets/logo.png')}
-            className="w-60 h-60"
+            className="h-60 w-60"
             resizeMode="contain"
           />
-          <Text className="text-white text-3xl font-bold mt-4">Welcome Back!</Text>
-          <Text className="text-gray-400 text-base mt-2">welcome back we missed you</Text>
+          <Text className="mt-4 text-3xl font-bold text-white">Welcome Back!</Text>
+          <Text className="mt-2 text-base text-gray-400">welcome back we missed you</Text>
         </View>
 
-        <View className="px-6 space-y-4">
+        <View className="space-y-4 px-6">
           <View className="space-y-2">
-            <Text className="text-gray-400 text-sm py-2 ml-1">Username</Text>
-            <View className="bg-gray-800 rounded-xl px-4 py-3 flex-row items-center">
+            <Text className="ml-1 py-2 text-sm text-gray-400">Username</Text>
+            <View className="flex-row items-center rounded-xl bg-gray-800 px-4 py-3">
               <TextInput
-                className="flex-1 text-white text-base"
+                className="flex-1 text-base text-white"
                 placeholder="Enter username"
                 placeholderTextColor="#666"
                 value={username}
@@ -93,15 +106,15 @@ export default function LoginScreen() {
               />
             </View>
             {errors.username && (
-              <Text className="text-red-500 text-sm ml-1">{errors.username}</Text>
+              <Text className="ml-1 text-sm text-red-500">{errors.username}</Text>
             )}
           </View>
 
           <View className="space-y-2">
-            <Text className="text-gray-400 text-sm py-2 ml-1">secretKey</Text>
-            <View className="bg-gray-800 rounded-xl px-4 py-3 flex-row items-center">
+            <Text className="ml-1 py-2 text-sm text-gray-400">secretKey</Text>
+            <View className="flex-row items-center rounded-xl bg-gray-800 px-4 py-3">
               <TextInput
-                className="flex-1 text-white text-base"
+                className="flex-1 text-base text-white"
                 placeholder="Enter secretKey"
                 placeholderTextColor="#666"
                 secureTextEntry={!showsecretKey}
@@ -109,21 +122,23 @@ export default function LoginScreen() {
                 onChangeText={setsecretKey}
               />
               <TouchableOpacity onPress={() => setShowsecretKey(!showsecretKey)}>
-                <Text className="text-gray-400">
-                  {showsecretKey ? '👁️' : '🙈'}
-                </Text>
+                <Text className="text-gray-400">{showsecretKey ? '👁️' : '🙈'}</Text>
               </TouchableOpacity>
             </View>
             {errors.secretKey && (
-              <Text className="text-red-500 text-sm ml-1">{errors.secretKey}</Text>
+              <Text className="ml-1 text-sm text-red-500">{errors.secretKey}</Text>
             )}
           </View>
 
           <TouchableOpacity
-            className="bg-yellow-700 rounded-xl py-4 items-center mt-4"
+            className="mt-4 items-center rounded-xl bg-yellow-700 py-4"
             onPress={handleLogin}
-          >
-            <Text className="text-white text-base font-semibold">Sign in</Text>
+            disabled={loading}>
+                {loading ? (
+                  <ActivityIndicator color="white" size="small" />
+                ) : (
+                  <Text className="text-white text-base font-semibold">Sign in</Text>
+                )}
           </TouchableOpacity>
         </View>
       </View>
