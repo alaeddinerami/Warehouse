2c777431cad06d37835ef938e0e4290ba118ac07
alaeddinerami <alaeddinerami7@gmail.com>
2025-02-12 14:52:21 +0100

    feat: fix routes

--- app/_layout.tsx
+++ app/_layout.tsx
@@ -9,9 +9,8 @@ export const unstable_settings = {
 
 export default function RootLayout() {
   return (
-    <Stack >
-      <Stack.Screen name="(tabs)" options={{ headerShown: false }} />
-      <Stack.Screen name="index" options={ {headerShown:false}} />
+    <Stack screenOptions={{ headerShown: false }} >
+      <Stack.Screen name="(tabs)"  />
     </Stack>
   );
 }
