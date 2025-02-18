038a91101e1df462c9556bd83f2909a0f22b36a9
alaeddinerami <alaeddinerami7@gmail.com>
2025-02-18 23:41:03 +0100

    Added jest.setup.js

---
+++ jest.setup.js
@@ -0,0 +1,14 @@
+import { Alert } from 'react-native';
+import * as ImagePicker from 'expo-image-picker';
+import apiClient from './(utils)/api'; 
+
+jest.mock('react-native', () => ({
+  Alert: {
+    alert: jest.fn(),
+  },
+}));
+
+
+jest.mock('./(utils)/api', () => ({
+  post: jest.fn(() => Promise.resolve({ data: { id: 1 } })),
+}));
